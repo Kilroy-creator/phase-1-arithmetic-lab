d3f9713e1183cc02b707378e5b47ab597b10ea41
Kilroy-creator <royhenry877@gmail.com>
2025-09-15 00:29:35 +0300

    first

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 62
 
-// Write your code here
+// Random integer > 0
+let random = Math.floor(Math.random() * 1000) + 1;
+
+// Modulus
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; // 4
+
+// Max
+let max = Math.max(1, 5, 10, 15, 20);
